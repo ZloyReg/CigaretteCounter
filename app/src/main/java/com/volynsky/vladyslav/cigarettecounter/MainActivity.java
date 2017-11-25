540332d5881a8442c1928bc0a53d4fcd231bc683
VladyslavVolynsky <VladyslavVolynsky@gmail.com>
2017-11-25 19:58:05 +0200

    Disable/Enable in separate methods Added the About button

--- app/src/main/java/com/volynsky/vladyslav/cigarettecounter/MainActivity.java
+++ app/src/main/java/com/volynsky/vladyslav/cigarettecounter/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.view.View.OnClickListener;
 import android.widget.Chronometer;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener{
 
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
     Button unfreezeB;
     Button smokeButton;
     Button resetButton;
+    Button aboutButton;
     Chronometer lastSmokeTimeChro;
 
     @Override
@@ -42,6 +44,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         smokeButton = findViewById(R.id.SmokeButton);
         resetButton = findViewById(R.id.ResetButton);
         lastSmokeTimeChro = findViewById(R.id.LastSmokeTimeChro);
+        aboutButton = findViewById(R.id.AboutButton);
     }
 
     public void setDefaultScreen() {
@@ -61,68 +64,73 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         unfreezeB.setOnClickListener(this);
         smokeButton.setOnClickListener(this);
         resetButton.setOnClickListener(this);
+        aboutButton.setOnClickListener(this);
+    }
+
+    public void buttonDisable (Button button){
+        button.setEnabled(false);
+        button.setTextColor(getResources().getColor(R.color.colorDisabled));
+    }
+
+    public void minusButtonEnable (Button button){
+        button.setEnabled(true);
+        button.setTextColor(getResources().getColor(R.color.colorAvailableNorm));
+    }
+
+    public void plusButtonEnable (Button button){
+        button.setEnabled(true);
+        button.setTextColor(getResources().getColor(R.color.colorAvailableBad));
+    }
+
+    public void buttonEnable (Button button){
+        button.setEnabled(true);
+        button.setTextColor(getResources().getColor(R.color.colorButtonText));
     }
 
     public void plusClicked (){
         norm++;
         normPerDayValue.setText(String.valueOf(norm));
-        minusB.setEnabled(true);
-        minusB.setTextColor(getResources().getColor(R.color.colorAvailableNorm));
-        freezeB.setEnabled(true);
-        freezeB.setTextColor(getResources().getColor(R.color.colorButtonText));
+        minusButtonEnable(minusB);
+        buttonEnable(freezeB);
         if (norm == 40) {
-            plusB.setEnabled(false);
-            plusB.setTextColor(getResources().getColor(R.color.colorDisabled));
+            buttonDisable(plusB);
         }
     }
 
     public void minusClicked (){
         norm--;
         normPerDayValue.setText(String.valueOf(norm));
-        plusB.setEnabled(true);
-        plusB.setTextColor(getResources().getColor(R.color.colorAvailableBad));
+        plusButtonEnable(plusB);
         if (norm < 1) {
-            minusB.setEnabled(false);
-            minusB.setTextColor(getResources().getColor(R.color.colorDisabled));
-            freezeB.setEnabled(false);
-            freezeB.setTextColor(getResources().getColor(R.color.colorDisabled));
-            smokeButton.setEnabled(false);
-            smokeButton.setTextColor(getResources().getColor(R.color.colorDisabled));
+            buttonDisable(minusB);
+            buttonDisable(freezeB);
+            buttonDisable(smokeButton);
         }
     }
 
     public void freezeClicked (){
-        smokeButton.setEnabled(true);
-        smokeButton.setTextColor(getResources().getColor(R.color.colorButtonText));
-        resetButton.setEnabled(true);
-        resetButton.setTextColor(getResources().getColor(R.color.colorButtonText));
+        buttonEnable(smokeButton);
+        buttonEnable(resetButton);
         availableToday = norm;
         availableForTodayText.setText(R.string.availableTodayText);
         availableForTodayText.append(String.valueOf(availableToday));
         smokedForTodayText.setText(R.string.smokedTodayText);
         smokedForTodayText.append(String.valueOf(smokedToday));
-        minusB.setEnabled(false);
-        minusB.setTextColor(getResources().getColor(R.color.colorDisabled));
-        plusB.setEnabled(false);
-        plusB.setTextColor(getResources().getColor(R.color.colorDisabled));
+        buttonDisable(minusB);
+        buttonDisable(plusB);
         freezeB.setEnabled(false);
+        buttonEnable(unfreezeB);
         unfreezeB.setVisibility(View.VISIBLE);
-        unfreezeB.setEnabled(true);
-        unfreezeB.setTextColor(getResources().getColor(R.color.colorButtonText));
         availableForTodayText.setTextColor(getResources().getColor(R.color.colorAvailableNorm));
         freezeB.setVisibility(View.INVISIBLE);
         freezeB.setEnabled(false);
     }
 
     public void unfreezeClicked (){
-        minusB.setEnabled(true);
-        minusB.setTextColor(getResources().getColor(R.color.colorAvailableNorm));
-        plusB.setEnabled(true);
-        plusB.setTextColor(getResources().getColor(R.color.colorAvailableBad));
-        freezeB.setEnabled(true);
-        freezeB.setTextColor(getResources().getColor(R.color.colorButtonText));
+        minusButtonEnable(minusB);
+        plusButtonEnable(plusB);
+        buttonEnable(freezeB);
         freezeB.setVisibility(View.VISIBLE);
-        freezeB.setEnabled(true);
         unfreezeB.setVisibility(View.INVISIBLE);
         unfreezeB.setEnabled(false);
     }
@@ -142,6 +150,10 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
             availableForTodayText.setTextColor(getResources().getColor(R.color.colorAvailableNorm));
         }
         lastSmokeTimeChro.start();
+    }
+
+    public void aboutClicked () {
+        Toast.makeText(this, R.string.aboutText, Toast.LENGTH_LONG).show();
     }
 
     public void resetClicked (){
@@ -173,6 +185,9 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
                     break;
                 case R.id.ResetButton:
                     resetClicked();
+                    break;
+                case R.id.AboutButton:
+                    aboutClicked();
                     break;
             }
     }
